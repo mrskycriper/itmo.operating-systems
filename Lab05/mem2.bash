977fa61654a4419e1c1f8afc4018eba7763a719e
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2020-12-12 13:11:36 +0300

    Create mem2.bash

---
+++ Lab05/mem2.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm report2.log
+
+count_step=0
+array_index=0
+array=()
+
+while true
+do
+  	for (( count=0; count<10; count++ ))
+        do
+          	array[$array_index]=$count
+                array_index=$((array_index+1))
+        done
+
+	count_step=$((count_step+1))
+        check=$count_step%100000
+
+        if [[ $check -eq "0" ]]
+        then
+            	echo "${#array[*]}" >> report2.log
+        fi
+done
